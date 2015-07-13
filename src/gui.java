ccb78b6b569174d86b725420e922165fb0894692
juliodieguez8a <juliodieguez8a@gmail.com>
2015-07-13 17:21:52 -0600

    ahi va

---
+++ src/gui.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+import java.util.ArrayList;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.text.*;
+import java.lang.*;
+
+public class gui extends JPanel{
+	
+	public gui() {
+	
+	}
+}
--- src/main.java
+++ src/main.java
@@ -2,18 +2,36 @@
  * 
  */
 
-/**
- * @author Julio
- *
- */
-public class main {
+//Jose Ramon Corona Boch
+//Samuel Diaz
+//Julio Dieguea Ochoa
+//13/7/15
+//HDT1
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+import javax.swing.JFrame;
 
+
+
+public final class main extends JFrame {
+
+    private final int OFFSET = 30;
+
+    public main() {
+        InitUI();
+    }
+
+    public void InitUI() {
+        gui board = new gui();
+        add(board);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 200);
+        setLocationRelativeTo(null);
+        setTitle("Radio_Sonica");
+    }
+
+    public static void main(String[] args) {
+        main v = new main();
+        v.setVisible(true);
+    }
 }
