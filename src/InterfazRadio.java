c561f33eff1c62cf508cdddaa9492344bd88ffe8
juliodieguez8a <juliodieguez8a@gmail.com>
2015-07-14 12:39:03 -0600

    conm

---
+++ src/InterfazRadio.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author Julio
+ *
+ */
+public interface InterfazRadio {
+	final int CONST_CAMBIO_AM = 10;
+	final double CONST_CAMBIO_FM = 0.2; 
+	final int MAX_AM = 1610;
+	final int MIN_AM = 530;
+	final double MAX_FM = 107.9;
+	final double MIN_FM = 87.9;
+	
+	public void encenderApagar();
+	
+	public void cambiarFrec();
+	
+	public void cargarEmisora(double emisora);
+	
+	public void adelantarEmisora();
+	
+	public void atrasarEmisora();
+	
+	public void guardarEmisora(double emisora, int posicion);
+	
+	public void subirVolumen();
+	
+	public void bajarVolumen();
+}
